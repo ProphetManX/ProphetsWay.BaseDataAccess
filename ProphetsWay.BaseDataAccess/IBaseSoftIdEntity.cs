d7f0f57d876b2d62c4457e2bca0da1e1dc5edb24
ProphetManX <35438914+ProphetManX@users.noreply.github.com>
2025-05-01 15:44:21 -0400

    added a new interface to specifiy fields to support a soft delete paradigm for entities. (#35)

---
+++ ProphetsWay.BaseDataAccess/IBaseSoftIdEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProphetsWay.BaseDataAccess
+{
+    public interface IBaseSoftIdEntity<T> : IBaseIdEntity<T>
+    {
+        DateTime CreatedDate { get; set; }
+        
+        DateTime? UpdatedDate { get; set; }
+
+        DateTime? DeletedDate { get; set; }
+
+    }
+}
